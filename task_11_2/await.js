0a67b3cba52698e5167b07c31ca3c00d7340ba94
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-04 14:31:49 +0200

    refact: comments on await task

--- task_11_2/await.js
+++ task_11_2/await.js
@@ -1,13 +1,17 @@
+//create a new async function
 async function fetchCats() {
+  //wrap the await commands inside a try catch incase there is an error
   try {
-    let catUrl = await fetch(
+    //after waiting for the fetch(await), drop the fetched data into catUrl
+    const catUrl = await fetch(
       'http://thecatapi.com/api/images/get?format=src&type=gif'
     );
-
+    //then log the url part of the catUrl
     console.log(catUrl.url);
   } catch (error) {
-    console.log(error);
+    //catch if there is an error
+    console.log(error); //log the error
   }
 }
-
+//run the function
 fetchCats();
