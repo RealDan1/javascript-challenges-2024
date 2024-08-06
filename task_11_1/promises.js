609a46d89248a85288e066029bc666d2b3f6b9e9
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-08-06 14:19:34 +0200

    refact: comments

--- task_11_1/promises.js
+++ task_11_1/promises.js
@@ -3,12 +3,12 @@ let pokemon = [];
 fetch('https://pokeapi.co/api/v2/pokemon/pikachu/')
   // convert JSON response to a object that we can use
   .then((res) => res.json())
-  // drop the returned object into result
+  // Drop the returned object into result
   .then((result) => {
-    // pass result(the object) into pokemon array
+    // Pass result(the object) into pokemon array
     pokemon = result;
 
-    // log the details of the pokemon object neatly
+    // Log the details of the pokemon object neatly
     console.log(
       `Name:\n${pokemon.name}
         \nWeight:\n${pokemon.weight}
